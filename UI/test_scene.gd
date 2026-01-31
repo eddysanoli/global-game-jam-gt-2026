09005f199fc14882f1e5f9c3c1604fd90e8142be
roderyx <roderyj@gmail.com>
2026-01-31 17:20:19 -0600

    pause menu

--- UI/test_scene.gd
+++ UI/test_scene.gd
@@ -1,5 +1,8 @@
 extends Node2D
 
+@onready var killMenu = $KillViewnew
+@onready var pauseMenu = $PauseMenu
+var paused = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,7 +12,24 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+	
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("Pause"):
+		pause()
+
+func pause() -> void:
+	if paused:
+		pauseMenu.hide()
+		Engine.time_scale = 1
+	else: 
+		pauseMenu.show()
+		Engine.time_scale = 0
+	paused = !paused
 
 
 func _on_monigote_monigote_clicked(node: Variant) -> void:
 	print(node.name)
+
+
+func _on_pause_menu_set_pause(bool: Variant) -> void:
+	pause()
