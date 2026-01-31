27948568aa88d4bf42f7baee1d3b990203bcbebc
Dredrin <feliperamirez003@gmail.com>
2026-01-31 17:06:05 -0600

    BG Music added

--- UI/main_menu.gd
+++ UI/main_menu.gd
@@ -4,6 +4,7 @@ func _ready():
 	%startBtn.pressed.connect(play)
 	%exitBtn.pressed.connect(quit)
 	Global.num_people = 20
+	BgMusic.play()
 
 func play(): 
 	get_tree().change_scene_to_file("res://scenes/Floor1/floor1.tscn")
