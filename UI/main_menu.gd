2827a315752fbc32a7086d03cb2bcd9dac906165
Dredrin <feliperamirez003@gmail.com>
2026-01-31 19:39:18 -0600

    Fonts updated

--- UI/main_menu.gd
+++ UI/main_menu.gd
@@ -4,10 +4,10 @@ func _ready():
 	%startBtn.pressed.connect(play)
 	%exitBtn.pressed.connect(quit)
 	Global.num_people = 20
+	Global.countdown = 300
 	BgMusic.play()
 
 func play(): 
-	Global.countdown = 10
 	get_tree().change_scene_to_file("res://scenes/Floor1/floor1.tscn")
 	Engine.time_scale = 1
 
--- global.gd
+++ global.gd
@@ -4,4 +4,4 @@ var garlic_basement: Vector2
 var garlic_l1: Vector2
 var garlic_l2: Vector2
 var num_people: int = 20
-var countdown: int
+var countdown: int = 300
