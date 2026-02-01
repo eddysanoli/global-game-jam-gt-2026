8b8fbbf30dcff43075f96c6ad780c20a802f2ec4
Alejandro <windevoxhelb@gmail.com>
2026-01-31 18:00:09 -0600

    NPC behaivour
    Se programo varios comportamientos para los NPCs, tal como caminar, esperar o cambiar de piso

--- scenes/entities/npc.gd
+++ scenes/entities/npc.gd
@@ -3,14 +3,21 @@ extends CharacterBody2D
 @export var speed_min: float = 40
 @export var speed_max: float = 80
 @export var memory_size: int = 5
+@export var repeated_actions: int = 3
 
 @onready var nav2d: NavigationAgent2D = $NavigationAgent2D
 @onready var collisionRadius = $CharacterCollision.shape.radius
 @onready var navArea = $MovementArea.shape.radius
 
+var action_count = 0
 var speed: float = 50
 var pastWaypoints: Array = []
 var similarityCounter := 0
+var actions = []
+var nearStairs = false
+var action = ["walk", "stand", "climbStairs"]
+var climbingStairs = false
+var standing = false
 
 func _ready() -> void:
 	set_physics_process(false)
@@ -27,6 +34,9 @@ func setup():
 
 func _physics_process(_delta):
 	manual_navigation()
+	if standing == true:
+		await get_tree().create_timer(3).timeout
+		standing = false
 	navigate_safe()
 	
 func manual_navigation():
@@ -36,7 +46,8 @@ func manual_navigation():
 	
 func navigate():
 	if nav2d.is_navigation_finished():
-		pickRandomPoint(global_position, collisionRadius, navArea)
+		await get_tree().create_timer(3).timeout
+		behaviour()
 	var next_path_position: Vector2 = nav2d.get_next_path_position()
 	velocity = (global_position.direction_to(next_path_position)* speed)
 #	Rotar sprite aqui
@@ -44,7 +55,10 @@ func navigate():
 	
 func navigate_safe():
 	if nav2d.is_navigation_finished():
-		pickRandomPoint(global_position, collisionRadius, navArea)
+		if climbingStairs == true:
+			climbingStairs = false
+			move_floor()
+		behaviour()
 	var next_path_position: Vector2 = nav2d.get_next_path_position()
 	var new_velocity: Vector2 = (global_position.direction_to(next_path_position)* speed)
 	nav2d.velocity = new_velocity
@@ -59,19 +73,51 @@ func pickRandomPoint(center: Vector2, inner: float, outer: float):
 	var dist: float = randf_range(inner, outer)
 	var new_target = ((vec*dist).rotated(angle) + center)
 	if pastWaypoints.size() != 0:
-		if pastWaypoints.size() == 5:
+		if pastWaypoints.size() == memory_size:
 			pastWaypoints.pop_front()
 		var last_Waypoint = pastWaypoints.back()
 		var cosene = last_Waypoint.dot(new_target)/(last_Waypoint.length() * new_target.length())
 		if cosene >= 0.75:
 			similarityCounter += 1
 			if similarityCounter == 5:
-				print("triggered")
 				if angle < PI:
 					angle = randf_range(PI, TAU)
 				angle = randf_range(0, PI)
 				new_target = ((vec*dist).rotated(angle) + center)
 				cosene = last_Waypoint.dot(new_target)/(last_Waypoint.length() * new_target.length())
 	pastWaypoints.append(new_target)
-	print(pastWaypoints)
 	nav2d.target_position = new_target
+	
+func behaviour():
+	var random_action = action.pick_random()
+	if actions.size() != 0:
+		if actions.back() == "climbStairs":
+			random_action = "walk"
+		for element in actions:
+			if element == random_action:
+				action_count += 1
+				if action_count == 3 and random_action == "stand":
+					random_action = "walk"
+					action_count = 0
+		if actions.size() == repeated_actions:
+			actions.pop_front()
+	actions.append(random_action)
+	
+	match random_action:
+		"walk":
+			pickRandomPoint(global_position, collisionRadius, navArea)
+		"stand":
+			standing = true
+		"climbStairs":
+			navStairs()
+
+
+func navStairs():
+	if nearStairs == true:
+		climbingStairs = true
+		var new_target = $"../../Stairs".global_position
+		nav2d.target_position = new_target
+		navigate_safe()
+		
+func move_floor():
+	pass
