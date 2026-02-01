fa3f817aaf411e45f0b490c085dafbe99e6ba142
Dredrin <feliperamirez003@gmail.com>
2026-01-31 22:07:22 -0600

    Organización de las carpetas
    Se eliminaron aquellos que no se utilizaban.

--- l_1_to_b.gd
+++
@@ -1,7 +0,0 @@
-class_name L1toB extends Area2D
-
-
-
-func _on_body_entered(body: Node2D) -> void:
-	var scene_tree = get_tree()
-	scene_tree.change_scene_to_file("res://scenes/Basement/basement.tscn") # Replace with function body.
